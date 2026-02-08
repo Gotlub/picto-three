6d06680d4130b693f0bad5e9ab6fd2601fde1451
Nicolas Frere <107085649+Gotlub@users.noreply.github.com>
2026-02-08 05:11:02 +0100

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ basedir = Path(__file__).parent.resolve()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    MAIL_USERNAME = = os.environ.get('MAIL_USERNAME') or 'you-will-never-guess'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'you-will-never-guess'
     RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
     RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
 
