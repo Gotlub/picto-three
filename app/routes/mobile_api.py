24e1e64c9e468865311a51e7383dfd8b7cbd955e
Nicolas FRERE <Taotoro@hotmail.com>
2026-05-08 00:47:31 +0200

    modifs miniature

--- app/routes/mobile_api.py
+++ app/routes/mobile_api.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify, current_app, send_from_directory
-from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity, get_jwt
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 from app.models import User, Tree, Image
 from app import db 
 import json
